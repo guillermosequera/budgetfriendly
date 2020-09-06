0a119b265c0d16efad67c502a2cfc275c05f0c73
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-06 01:54:13 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pregunta from './componentes/Pregunta';
+import Formulario from './componentes/Formulario';
+import Listado from './componentes/Listado'
 
 
 function App() {
+
+  //Definiendo el presupuesto
+  const [ presupuesto, guardarPresupuesto] = useState(0);
+  const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+  const [ gastos, guardarGastos ] = useState([]);
+
+  //cuando agreguemos un nuevo gasto
+  const agregarNuevoGasto = gasto => {
+    guardarGastos([
+      ...gastos,
+      gasto
+    ])
+  }
+
   return (
     <div className= "container">
       <header>
@@ -10,7 +27,29 @@ function App() {
         <h1>Gasto Semanal</h1>
 
         <div className="contenido-principal contenido">
-        <Pregunta />
+        { mostrarpregunta ? 
+          ( <Pregunta 
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+            actualizarPregunta={actualizarPregunta}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario 
+                  agregarNuevoGasto={agregarNuevoGasto}
+                />
+              </div>
+
+              <div className="one-half column">
+                <Listado 
+                  gastos={gastos}
+                />
+              </div>
+            </div>
+          )
+        }
+
         </div>
 
       </header>
--- src/componentes/Pregunta.js
+++ src/componentes/Pregunta.js
@@ -1,22 +1,56 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
+import Error from './Error'
 
-const Pregunta = () => {
+
+const Pregunta = ({ guardarPresupuesto, guardarRestante, actualizarPregunta }) => {
+
+    //definiendo el state
+    const [cantidad, guardarCantidad]= useState(0);
+    const[ error, guardarError ] = useState(false);
+
+    //funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad( parseInt(e.target.value, 10) )
+    }
+
+    //Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+        
+        //Validacion
+        if( cantidad < 1 || isNaN( cantidad ) ) {
+            guardarError(true);
+            return;
+        }
+
+        //si se pasa la validacion
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+    }
+
     return (
         <Fragment>
 
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            { error ? <Error mensaje="El presupuesto es incorrecto" /> : null }
+
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
 
                 <input
                     type="submit"
                     className="button-primary u-full-width"
-                    valiue="Definir Presupuesto"
+                    value="Definir Presupuesto"
                 />
 
             </form>
